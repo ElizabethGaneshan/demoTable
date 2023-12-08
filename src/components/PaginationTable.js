8965db25964de90bdde4be6243e42c6f07b8e1f4
Elizabethg <Elizabethg@gnrgy.com>
2023-12-08 16:52:30 +0530

    Using the helper function nextPage and previousPage to naviagte bewteen the previous and next page

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -13,36 +13,51 @@ export const PaginationTable = () => {
     getTableBodyProps,
     headerGroups, // a group of headers
     page,
+    nextPage, // helper function that react-table is giving us to navigating across different pages.
+    previousPage, // helper function that react-table is giving us to navigating across different pages.
     prepareRow,
   } = tableInstance;
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroups) => (
-          <tr {...headerGroups.getHeaderGroupProps()}>
-            {headerGroups.headers.map((column) => (
-              <th {...column.getHeaderProps()}>
-                {column.render("Header")}
-                <span>
-                  {column.isSorted ? (column.isSortedDesc ? "down" : "up") : ""}
-                </span>
-              </th>
+    return (
+      <>
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroups) => (
+              <tr {...headerGroups.getHeaderGroupProps()}>
+                {headerGroups.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                    <span>
+                      {column.isSorted
+                        ? column.isSortedDesc
+                          ? "down"
+                          : "up"
+                        : ""}
+                    </span>
+                  </th>
+                ))}
+              </tr>
             ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {page.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")} </td>;
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {page.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")} </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+            </table>
+            
+            <div>
+                <button onClick={() => previousPage()}>Previous</button>
+                <button onClick={()=> nextPage()}>Next</button>
+            </div>
+      </>
+    );
 };
